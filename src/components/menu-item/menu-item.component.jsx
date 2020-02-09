6c59d5f3932ec2c3e0afd0d7ec5eeadcacdc0ca2
allison <allisonchen1130@gmail.com>
2020-02-08 23:53:27 -0500

    Styling menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,11 @@ import "/Users/mengting/lighthouse/crwn-clothing/src/components/menu-item/menu-i
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
